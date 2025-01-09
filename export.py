0d7f9848786d0a566b80559e1f0ae83a71d3e604
XY_cpp <xy_cpp@icloud.com>
2025-01-09 13:07:22 +0800

    [fix]修复模型导出名字错误

--- export.py
+++ export.py
@@ -3,9 +3,9 @@ import sys
 import coremltools as ct
 from ultralytics import YOLO
 
-name = sys.argv[1].split('.')[0]
-imgsz = (640,640)
+name = sys.argv[1].split(".")[0]
+imgsz = (640, 640)
 model = YOLO(f"{name}.pt")
 
-model.export(format='mlmodel',imgsz=imgsz)
-os.system(f"xcrun coremlcompiler compile {name}.mlpackage ./")
+model.export(format="mlmodel", imgsz=imgsz)
+os.system(f"xcrun coremlcompiler compile {name}.mlmodel ./")
